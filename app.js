42a54c762d7882075c8cbc3c32a02d93a0c599cd
rhonnyjaimes <rhonnyjaimes2002@gmail.com>
2024-08-13 18:02:24 -0400

    Se agregan rutas mas optimizadas para la funcionalidad para las vistas

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const estudiantesRoutes = require('./routes/estudiantesroutes');
 
 // Crear una instancia de Express
@@ -12,17 +13,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Configurar method-override para permitir métodos PUT y DELETE en formularios
+app.use(methodOverride('_method'));
+
 // Configuración de la carpeta de vistas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Rutas para estudiantes
 app.use('/estudiantes', estudiantesRoutes);
 
+// Ruta principal
 app.get('/', (req, res) => {
   const estudiantes = require('./data/estudiantes'); // Cargar los estudiantes aquí
   res.render('index', { estudiantes });
 });
 
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
