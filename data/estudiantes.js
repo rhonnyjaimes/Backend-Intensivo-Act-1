271943c75f5596108cc21341b5aae9e9687dab7c
rhonnyjaimes <rhonnyjaimes2002@gmail.com>
2024-08-13 14:00:22 -0400

    definicion de data y models

---
+++ data/estudiantes.js
@@ -0,0 +1,18 @@
+const Estudiante = require('../models/estudiantes');
+
+let estudiantes = [
+    new Estudiante(1, 'Rhonny Jaimes', '29718095', 'rhonny@uvm.com'),
+    new Estudiante(2, 'María Gómez', '25634092', 'maria@uvm.com'),
+    new Estudiante(3, 'Yerson Perez', '30748234', 'yerson@uvm.com'),
+    new Estudiante(4, 'Luis García', '27894512', 'luis.garcia@uvm.com'),
+    new Estudiante(5, 'Ana Rodríguez', '29987654', 'ana.rodriguez@uvm.com'),
+    new Estudiante(6, 'Carlos Sánchez', '28876543', 'carlos.sanchez@uvm.com'),
+    new Estudiante(7, 'Elena Martínez', '27654321', 'elena.martinez@uvm.com'),
+    new Estudiante(8, 'Fernando Silva', '26789012', 'fernando.silva@uvm.com'),
+    new Estudiante(9, 'Gabriela Torres', '28765432', 'gabriela.torres@uvm.com'),
+    new Estudiante(10, 'Diego López', '27891234', 'diego.lopez@uvm.com'),
+    new Estudiante(11, 'Verónica Díaz', '29874321', 'veronica.diaz@uvm.com')
+];
+
+
+module.exports = estudiantes;
---
+++ models/estudiantes.js
@@ -0,0 +1,10 @@
+class Estudiante {
+    constructor(id, nombre, cedula, correoElectronico) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correoElectronico = correoElectronico;
+    }
+}
+
+module.exports = Estudiante;
